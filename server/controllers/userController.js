c081cdcc886041f0bdf16c807650c7c1acd4ec98
Harshit Singh <harshitsinghprof@gmail.com>
2025-06-22 19:29:55 +0530

    backend done 3.47

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ import { request } from "express"
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
+import cloudinary from "../lib/cloudinary.js";
 
 // Signup a new user
 export const signup = async(req,res)=>{
@@ -31,5 +32,56 @@ export const signup = async(req,res)=>{
         res.json({success:false,message:error.message})
     }
 } 
+
+// Controller to login a user
+
+export const login = async(req,res) => {
+    try {
+        const { email , password} = req.body;
+        const userData = await User.findOne({email})
+
+        const isPasswordCorrect = await bcrypt.compare(password,userData.password);
+
+        if(!isPasswordCorrect){
+            return res.json({success:false,message:"Invalid credentials"});
+        }
+        
+        const token = generateToken(userData._id)
+        res.json({success:true,userData,token,message:"Login successful"})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false,message:error.message})
+    }
+}
+
+// Controller to check if user is authenticated
+export const checkAuth = (req,res) => {
+    res.json({success:true ,user:req.user});
+}
+
+// Controller to update user profile details
+export const updateProfile = async (req,res)=>{
+    try {
+        const { profilePic , bio , fullName } = req.body;
+
+        const userId = req.user._id;
+        let updatedUser;
+
+        if(!profilePic){
+           updatedUser =  await User.findByIdAndUpdate(userId,{bio,fullName},{new:true});
+        }
+        else{
+            const uplode = await cloudinary.uploader.upload(profilePic);
+
+            updatedUser = await User.findByIdAndUpdate(userId , {profilePic : upload.secure_url , bio , fullName},{new:true});
+        }
+        res.json({success:true,user:updatedUser})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false,message:error.message})
+    }
+}
+
         
 
